5bc851dbd8bc86687ff7cea15b2f308d42c64c55
RosarioRu <mariarruval@gmail.com>
2022-08-21 11:40:19 -0700

    Add joint CategoryUserRecipe class/model.

--- MyRecipes/Models/CategoryUserRecipe.cs
+++ MyRecipes/Models/CategoryUserRecipe.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
 
+
 namespace MyRecipes.Models
 {
   public class CategoryUserRecipe
     {
-        
+      public int CategoryUserRecipeId { get; set; }
+      public int CategoryId { get; set; }
+      public int UserRecipeId { get; set; }
+      public virtual Category Category { get; set; }
+      public virtual UserRecipe UserRecipe { get; set; }
     }
 }
--- MyRecipes/Models/UserRecipe.cs
+++ MyRecipes/Models/UserRecipe.cs
@@ -1,9 +1,22 @@
-using System;
+using System.Collections.Generic;
 
 namespace MyRecipes.Models
 {
     public class UserRecipe
     {
-        
+      public UserRecipe()
+      {
+        this.JoinEntities = new HashSet<CategoryUserRecipe>;
+      }
+
+      public int UserRecipeId { get; set; }
+      public string RecipeName { get; set; }
+      public string IngredientList { get; set; }
+      public int RecipeRating { get; set; }
+
+      public virtual ICollection<CategoryUserRecipe> JoinEntities { get; }
+    }
+
+
     }
 }
